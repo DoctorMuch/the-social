2b16b772325aed399713afbf87c4ef7741d38a3b
David F Mueth <78443941+DoctorMuch@users.noreply.github.com>
2021-12-07 12:02:08 -0600

    working on Thought schema and date formatter

---
+++ models/Thought.js
@@ -0,0 +1,32 @@
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ThoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (createdAtVal) => dateFormat(createdAtVal)
+  },
+  username: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  reactions: [ReactionSchema]
+},
+  {
+    toJSON: {
+      getters: true
+    },
+    id: false
+  }
+);
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
