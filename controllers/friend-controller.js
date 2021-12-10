ce5b9afcad66dda02d517697e9a26f9c203a191d
David F Mueth <78443941+DoctorMuch@users.noreply.github.com>
2021-12-10 11:46:04 -0600

    fixed removeFriend so it did not delete the user when removing from friend list

--- controllers/friend-controller.js
+++ controllers/friend-controller.js
@@ -7,29 +7,22 @@ const friendController = {
       { $push: { friends: params.friendId } },
       { new: true }
     )
-    .then(dbUserData => {
-      if(!dbUserData) {
-        res.status(404).json({ message: 'No such user to add this friend.' });
-        return;
-      }
-      res.json(dbUserData);
-    })
-    .catch(err => res.status(500).json({ err: err.message }));
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No such user to add this friend.' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.status(500).json({ err: err.message }));
   },
 
   removeFriend({ params }, res) {
-    User.findOneAndDelete({ _id: params.friendId })
-      .then(deletedFriend => {
-        if (!deletedFriend) {
-          res.status(404).json({ message: 'No friend found with this ID found in friend list.' });
-          return;
-        }
-        return User.findOneAndUpdate(
-          { _id: params.userId },
-          { $pull: { friends: params.friendId } },
-          { new: true }
-        );
-      })
+    User.findOneAndUpdate(
+      { _id: params.userId },
+      { $pull: { friends: params.friendId } },
+      { new: true }
+    )
       .then(dbUserData => {
         if (!dbUserData) {
           res.status(404).json({ message: 'No user in our list of registered users with this ID.' });
